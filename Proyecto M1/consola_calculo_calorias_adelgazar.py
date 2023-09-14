f9b6e06baa3f5599a33f32f2a68c403d80ba205a
Eduardo León <66545075+eduardoleon9010@users.noreply.github.com>
2023-09-14 09:57:25 -0500

    Update consola_calculo_calorias_adelgazar.py

--- Proyecto M1/consola_calculo_calorias_adelgazar.py
+++ Proyecto M1/consola_calculo_calorias_adelgazar.py
@@ -1,27 +1,27 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jan  5 23:10:12 2022
+Created on Sat Sep  9 20:10:59 2023
 
-@author: abelator
-
-Con este archivo interactuamos con el usuario para obtener los datos
-necesarios para caluclar las calorias necesarias según la actividad física y devolverselo al usuario
+@author: Ing. Leon
 """
 
 import calculadora_indices as calc
-"""
-Con la linea anterior importamos el módulo que realiza todos
-los cálculos de los diferentes índices
-"""
 
-def calorias_para_adelgazar() -> None:
-    
-    peso = float(input("Ingrese el peso de la persona (en kilogramos): "))
-    altura = float(input("Ingresa la altura de la persona (en centimetros): "))
-    edad = int(input("Ingresa la edad de la persona (en años): "))
-    valor_genero = int(input("Ingresa el valor 5 en caso de ser masculino o -161 en caso de ser femenino: "))
-    cal_x_adelg = calc.consumo_calorias_recomendado_para_adelgazar(peso, altura, edad, valor_genero)
-    print ("\nPara adelgazar es recomendado que consumas entre:  " + (cal_x_adelg) + " calorías al día")
-    
-calorias_para_adelgazar()
+print("Calculadora de Calorías para Adelgazar")
+peso = float(input("Ingrese su peso en kilogramos: "))
+altura = float(input("Ingrese su altura en centímetros: "))
+edad = int(input("Ingrese su edad en años: "))
+sexo = input("Ingrese su sexo (H para hombre, M para mujer): ").upper()
+
+if sexo == 'H':
+    valor_genero = 5
+elif sexo == 'M':
+    valor_genero = -161
+else:
+    print("Sexo no válido")
+    exit()
+
+rango_calorias = calc.consumo_calorias_recomendado_para_adelgazar(peso, altura, edad, valor_genero)
+print(rango_calorias)
+
 
