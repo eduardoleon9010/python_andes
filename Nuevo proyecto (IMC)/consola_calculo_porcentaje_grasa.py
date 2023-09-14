1883c558fd980ab805901959c8e7f66f11d07977
Eduardo León <66545075+eduardoleon9010@users.noreply.github.com>
2023-09-14 11:35:13 -0500

    Delete Nuevo proyecto (IMC) directory

--- Nuevo proyecto (IMC)/consola_calculo_porcentaje_grasa.py
+++
@@ -1,7 +0,0 @@
-import formulas as form
-peso=float(input('ingrese el peso de la persona(KG) '))
-altura=float(input('ingrese la estatura de la persona(M) '))
-edad=int(input('ingrese la edad de la persona '))
-valor_genero=float(input('ingrese el valor 10.8 en caso de ser hombre si no es asi ingresar el valor 0 '))
-p=1.2*form.calcular_IMC(peso, altura)+0.23*edad-5.24-valor_genero
-print(p)
