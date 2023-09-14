abd6f9c75784401c0d20286ea3607b49c3e6501c
Eduardo León <66545075+eduardoleon9010@users.noreply.github.com>
2023-09-14 09:59:20 -0500

    Update consola_calculo_porcentaje_grasa.py

--- Proyecto M1/consola_calculo_porcentaje_grasa.py
+++ Proyecto M1/consola_calculo_porcentaje_grasa.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jan  5 22:17:28 2022
+Created on Sat Sep  9 20:10:18 2023
 
-@author: abelator
-
-Con este archivo interactuamos con el usuario para obtener los datos
-necesarios para caluclar el porcentaje de grasa corporal y devolverselo al usuario
+@author: Ing. Leon
 """
 
 import calculadora_indices as calc
-"""
-Con la linea anterior importamos el módulo que realiza todos
-los cálculos de los diferentes índices
-"""
 
-def porcentage_grasa() -> None:
-    
-    peso = float(input("Ingrese el peso de la persona (en kilogramos): "))
-    altura = float(input("Ingresa la altura de la persona (en metros): "))
-    edad = int(input("Ingresa la edad de la persona (en años): "))
-    valor_genero = float(input("Ingresa el valor 10.8 en caso de ser masculino o 0 en caso de ser femenino: "))
-    porcen_grasa = calc.calcular_porcentaje_grasa(peso, altura, edad, valor_genero)
-    print ("\nEl porcentaje de grasa de la persona es: " + str(round(porcen_grasa, 2)) + "%")
-    
-porcentage_grasa()
+print("Calculadora de Calorías para Adelgazar")
+peso = float(input("Ingrese su peso en kilogramos: "))
+altura = float(input("Ingrese su altura en centímetros: "))
+edad = int(input("Ingrese su edad en años: "))
+genero = input("Ingrese su género (H para hombre, M para mujer): ").upper()
+
+if genero == 'H':
+    valor_genero = 5
+elif genero == 'M':
+    valor_genero = -161
+else:
+    print("Género no válido")
+    exit()
+
+rango_calorias = calc.consumo_calorias_recomendado_para_adelgazar(peso, altura, edad, valor_genero)
+print(rango_calorias)
