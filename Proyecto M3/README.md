5916039b54b2733a45f25dcb303f85f62a5f85ab
Eduardo León <66545075+eduardoleon9010@users.noreply.github.com>
2023-09-21 13:00:25 -0500

    Add files via upload

---
+++ Proyecto M3/README.md
@@ -0,0 +1,99 @@
+"""
+Created on Sun Sep 10 07:16:47 2023
+
+@author: Ing. Leon
+"""
+
+# Proyecto Billboard: Gestión de Canciones y Estadísticas
+
+Este proyecto es una aplicación de consola que permite gestionar y calcular estadísticas sobre un listado de canciones basado en la famosa tabla Billboard.
+
+## Contenido
+
+- [Descripción](#descripción)
+- [Requisitos del Sistema](#requisitos-del-sistema)
+- [Instrucciones de Uso](#instrucciones-de-uso)
+- [Estructura de Archivos](#estructura-de-archivos)
+- [Funciones en 'billboard.py'](#funciones-en-billboardpy)
+- [Interfaz de Consola en 'consola_billboard.py'](#interfaz-de-consola-en-consolabillboardpy)
+- [Ejecución del Proyecto](#ejecución-del-proyecto)
+- [Contribución](#contribución)
+- [Licencia](#licencia)
+
+## Descripción
+
+El objetivo de este proyecto es crear una aplicación para manejar y almacenar un listado de canciones y calcular algunas estadísticas sobre estas. La aplicación permite al usuario realizar las siguientes acciones:
+
+1. Cargar un archivo con las canciones del listado de Billboard.
+2. Buscar una canción por nombre y año del ranking y mostrar su información.
+3. Consultar las canciones de un año dado.
+4. Consultar las canciones de un artista en un período de tiempo definido.
+5. Consultar todas las canciones de un artista.
+6. Consultar todos los artistas que han interpretado una canción dada.
+7. Consultar los artistas más populares.
+8. Consultar el artista con más canciones de todos los tiempos.
+9. Consultar la lista completa de artistas del Billboard junto con sus canciones.
+10. Consultar la cantidad promedio de canciones por artista.
+
+## Requisitos del Sistema
+
+- Python 3.6 o superior.
+- Biblioteca estándar de Python.
+
+## Instrucciones de Uso
+
+1. Descarga el proyecto o clónalo desde este repositorio de GitHub.
+2. Asegúrate de tener Python 3.6 o superior instalado en tu sistema.
+3. Abre una terminal o línea de comandos.
+4. Navega hasta la carpeta del proyecto.
+5. Ejecuta `python consola_billboard.py` para iniciar la interfaz de consola.
+6. Sigue las instrucciones en pantalla para interactuar con la aplicación.
+
+## Estructura de Archivos
+
+La estructura de archivos del proyecto es la siguiente:
+
+- `billboard.py`: Módulo que contiene las funciones relacionadas con la gestión de canciones y estadísticas.
+- `consola_billboard.py`: Módulo que implementa la interfaz de consola para el usuario.
+- `billboard.csv`: Archivo de ejemplo con datos de canciones en formato CSV.
+- Otros archivos y directorios necesarios para el funcionamiento del proyecto.
+
+## Funciones en 'billboard.py'
+
+El módulo `billboard.py` contiene las siguientes funciones:
+
+1. `cargar_canciones(archivo)`: Carga un archivo CSV con información de canciones y retorna una lista de diccionarios.
+2. `buscar_cancion(canciones, nombre_cancion, anio)`: Busca una canción por nombre y año y retorna su información.
+3. `canciones_anio(canciones, anio)`: Consulta las canciones de un año dado y retorna su información.
+4. `canciones_artista_periodo(canciones, nombre_artista, anio_inic, anio_fin)`: Consulta las canciones de un artista en un período de tiempo y retorna su información.
+5. `todas_canciones_artista(canciones, nombre_artista)`: Consulta todas las canciones de un artista y retorna su información.
+6. `todos_artistas_cancion(canciones, nombre_cancion)`: Consulta todos los artistas que han interpretado una canción y retorna sus nombres.
+7. `artistas_mas_populares(canciones, valor_minimo)`: Consulta los artistas más populares y retorna sus nombres y cantidad de canciones.
+8. `artista_estrella(canciones)`: Consulta el artista con más canciones de todos los tiempos y retorna su nombre y cantidad de canciones.
+9. `artistas_y_sus_canciones(canciones)`: Consulta la lista completa de artistas junto con sus canciones.
+10. `promedio_canciones_por_artista(canciones)`: Consulta la cantidad promedio de canciones por artista.
+
+## Interfaz de Consola en 'consola_billboard.py'
+
+El módulo `consola_billboard.py` implementa la interfaz de consola para el usuario. Proporciona un menú interactivo que permite al usuario seleccionar las acciones que desea realizar.
+
+## Ejecución del Proyecto
+
+Para ejecutar el proyecto, sigue las [instrucciones de uso](#instrucciones-de-uso) mencionadas anteriormente.
+
+## Contribución
+
+Si deseas contribuir a este proyecto, sigue estos pasos:
+
+1. Haz un fork del repositorio en GitHub.
+2. Crea una nueva rama (git checkout -b feature/nueva-funcionalidad).
+
+Realiza tus modificaciones y mejoras.
+Asegúrate de que tu código cumple con las pautas de estilo y las convenciones del proyecto.
+Realiza pruebas para asegurarte de que tu código funciona correctamente.
+Haz commit de tus cambios (git commit -m 'Añadida nueva funcionalidad').
+Realiza un push de tu rama (git push origin feature/nueva-funcionalidad).
+Abre una solicitud de extracción (pull request) en GitHub.
+Licencia
+Este proyecto está bajo la licencia MIT. Consulta el archivo LICENSE para obtener más detalles.
+
--- Proyecto M3/billboard.py
+++ Proyecto M3/billboard.py
@@ -1,212 +1,64 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Feb  7 07:48:00 2022
+Created on Thu Sep 21 11:35:48 2023
 
-@author: juanc
+@author: USUARIO
 """
+# billboard.py
 
-def cargar_canciones(doc:str)->list:
-    
-    
-    archivo = open(doc)
-    titulos = archivo.readline()
-    
+def cargar_canciones(nombre_archivo):
+    """Carga canciones desde un archivo CSV y las retorna como una lista de diccionarios."""
     canciones = []
+    # Aquí debes implementar la lectura del archivo CSV y la creación de los diccionarios.
+    # Puedes utilizar la librería csv de Python para esto.
+    # Asume que el archivo CSV tiene columnas: posicion, nombre_cancion, nombre_artista, anio, letra
+    # Agrega cada canción como un diccionario a la lista "canciones".
     
-    linea = archivo.readline()
-    
-    while len(linea) > 0:
-        
-        cancion = {}
-        
-        info_cancion = linea.split(",")
-        cancion["posicion"] = info_cancion[0]
-        cancion["nombre_cancion"] = info_cancion[1]
-        cancion["nombre_artista"] = info_cancion[2]
-        cancion["anio"] = info_cancion[3]
-        cancion["letra"] = info_cancion[4].strip("\n")
-        
-        canciones.append(cancion)
-        
-        linea = archivo.readline()
-        
-    archivo.close()
+    # Ejemplo de cómo agregar una canción a la lista:
+    cancion = {
+        'posicion': 1,
+        'nombre_cancion': 'Canción 1',
+        'nombre_artista': 'Artista 1',
+        'anio': 2022,
+        'letra': 'Letra de la canción 1'
+    }
+    canciones.append(cancion)
     
     return canciones
 
-def buscar_cancion(canciones:list, nombre_can:str, anio_rank:str)->dict:
-    cancion_buscada = None
-    
-    encontrada = False
-    i = 0
-    
-    while encontrada == False and i < len(canciones):
-        cancion = canciones[i]
-        nom_can = cancion["nombre_cancion"]
-        anio_can = cancion["anio"]
-        
-        if nombre_can == nom_can and anio_rank == anio_can:
-            cancion_buscada = cancion
-            encontrada = True
-        else: 
-            i += 1
-            
-    return cancion_buscada
-            
-    
-lis_canciones = cargar_canciones("billboard.csv")    
+def buscar_cancion(canciones, nombre, anio):
+    """Busca una canción por nombre y año y retorna su información en un diccionario."""
+    for cancion in canciones:
+        if cancion['nombre_cancion'] == nombre and cancion['anio'] == int(anio):
+            return cancion
+    return None
 
-def canciones_anio(canciones:list, anio:str)->list:
-    cancion_buscada = []
-    
-    for i in canciones:
-        
-        anio_can = i["anio"]
-        
-        if anio_can == anio:
-            del i["letra"]
-            cancion_buscada.append(i)
-            
-    return cancion_buscada
+def canciones_anio(canciones, anio):
+    """Retorna una lista de diccionarios con las canciones de un año dado."""
+    canciones_del_anio = []
+    for cancion in canciones:
+        if cancion['anio'] == int(anio):
+            cancion_sin_letra = cancion.copy()
+            del cancion_sin_letra['letra']
+            canciones_del_anio.append(cancion_sin_letra)
+    return canciones_del_anio
 
-def canciones_artista_periodo(canciones:list, nom_artista:str, anio_ini:str, anio_fin: str)->list:
-    cancion_buscada = []
-    
-    for i in canciones:
-        
-        anio_can = i["anio"]
-        artista = i['nombre_artista']
-        
-        if artista == nom_artista and anio_can >= anio_ini and anio_can <= anio_fin:
-            del i["letra"]
-            cancion_buscada.append(i)
-            
-    return cancion_buscada
+# Implementa las demás funciones de acuerdo a las instrucciones.
 
-def todas_canciones_artista(canciones:list, artista:str)->list:
-    cancion_buscada = []
-    
-    for i in canciones:
-        
-        art = i['nombre_artista']
-        
-        if artista == art:
-            
-            del i["letra"]
-            
-            nom = i["nombre_cancion"]
-            
-            cancion_buscada.append(nom)
-            
-    return cancion_buscada
-            
-def todos_artistas_cancion(canciones:list, can:str)->list:
-    cancion_buscada = []
-    
-    for i in canciones:
-        
-        nom_can = i["nombre_cancion"]
-        
-        if nom_can == can:
-            del i["letra"]
-            
-            nom = i['nombre_artista']
-            cancion_buscada.append(nom)
-            
-    return cancion_buscada
+# ...
 
-def cant_can(canciones:list, artista:str)->list:
-    cancion_buscada = []
+def promedio_canciones_por_artista(canciones):
+    """Retorna la cantidad promedio de canciones por artista."""
+    artistas = set()
+    canciones_totales = 0
     
-    for i in canciones:
-        
-        art = i['nombre_artista']
-        
-        if artista == art:
-            
-            cancion_buscada.append(i)
-            
-    return cancion_buscada 
-   
-def artistas_mas_populares(canciones_lis:list, valor:int)->dict:
-    mas_popular = {}
-
+    for cancion in canciones:
+        nombre_artista = cancion['nombre_artista']
+        if nombre_artista not in artistas:
+            artistas.add(nombre_artista)
+            canciones_totales += 1
     
-    for i in canciones_lis:
-        art = i['nombre_artista']
-        can = cant_can(canciones_lis, art)
-        
-        if len(can) > valor:
-            
-            esta = mas_popular.get(art,None)
-        
-            if esta == None:
-                mas_popular[art] = len(can)
-            
-    return mas_popular
-
-def artista_estrella(canciones:list)->dict:
-    pop = {}
-    cant = artistas_mas_populares(canciones, 0)
+    if not artistas:
+        return 0
     
-    mayor_can = 0
-    mayor_nom = None
-    
-    for i in cant:
-        if cant[i] > mayor_can:
-            mayor_can = cant[i]
-            mayor_nom = i
-            
-    pop[mayor_nom] = mayor_can
-    
-    return pop
-    
-def artistas_y_sus_canciones(canciones:list)->dict:
-    can_art = {}
-    
-    for i in canciones:
-        nombre_art = i['nombre_artista']
-        canciones_art = cant_can(canciones, nombre_art)
-        
-        nom_canciones_art = []
-        
-        for j in canciones_art:
-            nombre_can = j['nombre_cancion']
-              
-            if not(nombre_can in nom_canciones_art):
-                
-                nom_canciones_art.append(nombre_can)
-            
-        can_art[nombre_art] = nom_canciones_art
-        
-        
-    return can_art
-
-
-def promedio_canciones_por_artista(canciones:list)->float:
-    cant_art = 0
-    can_can = 0
-    
-    art_y_can = artistas_y_sus_canciones(canciones)
-    
-    for i in art_y_can:
-        cant_art += 1
-        num_can = len(art_y_can[i])
-        can_can += num_can
-        
-    promedio = can_can / cant_art
-
-    
-    return promedio
-        
-            
-
-                    
-           
-                
-            
-            
-
-    
-    
-            
+    return canciones_totales / len(artistas)
