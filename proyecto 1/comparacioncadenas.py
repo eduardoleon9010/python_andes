229bb7be95ea573cb01f78ac92b4d619f052e466
Ing. Eduardo León <66545075+eduardoleon9010@users.noreply.github.com>
2024-02-07 09:21:46 -0500

    Delete proyecto 1 directory

--- proyecto 1/comparacioncadenas.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-def comparar_cadenas(palabra1: str, palabra2: str,)->None:
-    if (palabra1 == palabra2):
-        print("Las palabras son uguales")
-    elif (palabra1 < palabra2):
-        print("Las palabras",palabra1,"es meneos quela palabra",palabra2)
-    else:
-        print("La palabra",palabra1,"es mayor que la palabra",palabra2)
--- proyecto 1/condicionales_purntos colineales.py
+++
@@ -1,6 +0,0 @@
-def son_colineales(x1:int,y1:int,x2:int,y2:int,x3:int,y3:int)->bool:
-    pendiente_1 = (y2-y1)/(x2-x1)
-    pendiente_2 = (y3-y2)/(x3-x2)
-    
-    return pendiente_1 == pendiente_2
-  
